a4a04b054bdfd2e735d5d2b54a9e44fee0eef98e
Aleksey Andreev <andreev1984@bk.ru>
2024-06-22 16:40:25 +0300

    lesson1/Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 2;
+int i2 = 3;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+Console.WriteLine(sum3);
+
+int product = i1 * i1;
+long product2 = i1 * l1;
+double product3 = i1 * d1;
+Console.WriteLine(product);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
