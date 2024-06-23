c7c58d609eb7bec93bb188d8a5fddbabf64947b7
Aleksey Andreev <andreev1984@bk.ru>
2024-06-23 11:51:10 +0300

    add task7/Lesson2

---
+++ task2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+while (i < N)
+{
+    arr[i] = i + 1;
+    //Console.WriteLine(arr[i]);
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = 10;
+int[] arr = new int[N];
+int i = 0;
+
+while (i < N)
+{
+    arr[i] = i + 1;
+    if (arr[i] % 2 ==0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
