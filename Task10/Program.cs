d85f310a3f6907f14177861bbacabfc94e9f5329
Aleksey Andreev <andreev1984@bk.ru>
2024-06-23 18:18:30 +0300

    add lesson3/Task2

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void squareOfNumbers(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+squareOfNumbers(5);
+squareOfNumbers(10);
+squareOfNumbers(15);
