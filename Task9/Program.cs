1da491cb26d2c97cef12c53f95aef72d833d81bd
Aleksey Andreev <andreev1984@bk.ru>
2024-06-23 17:47:32 +0300

    add Lesson3/task1

---
+++ Task9/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+double formula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int domenator = c + d;
+
+    double rez = numenator / domenator;
+    return rez;
+}
+
+double rezult = formula(1, 2, 3, 4);
+Console.WriteLine(rezult);
